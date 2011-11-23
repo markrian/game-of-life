2c3f78cd0d27699aee53999354251515df3e4a8f
Mark Florian <markrian@gmail.com>
2011-11-23 00:30:22 +0000

    Flesh out nextState method.

--- life.js
+++ life.js
@@ -4,14 +4,25 @@ function Cell() {
     this.neighbours = {};
 }
 
-Cell.prototype.getNextState = function () {
-    var livingNeighbours = 0,
+Cell.prototype.nextState = function () {
+    var n = 0,
         neighbour;
     for (neighbour in this.neighbours) {
         if (this.neighbours.hasOwnProperty(neighbour)) {
             // If a neighbour is alive, then increment livingNeighbours by 1, otherwise, by 0.
-            livingNeighbours += +this.neighbours[neighbour].alive;
+            n += +this.neighbours[neighbour].alive;
         }
+    }
+    if (this.alive) {
+        if (n < 2) {
+            this.aliveNext = false;
+        } else if (n < 4) {
+            this.aliveNext = true;
+        } else if (n >= 4) {
+            this.aliveNext = false;
+        }
+    } else if (n == 3) {
+        this.aliveNext = true;
     }
 };
 
