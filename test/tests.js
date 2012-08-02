5a798f4b0957c659bb6473bb3c05d31d772294a1
Mark Florian <markrian@gmail.com>
2012-08-03 01:26:19 +0200

    Add more Cell tests

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,7 @@
+function randomSample() {
+	return arguments[Math.floor(Math.random() * arguments.length)];
+}
+
 module( "Cells" );
 
 test( "Basics", function () {
@@ -11,15 +15,43 @@ test( "Basics", function () {
 	cell.die();
 	cell.age();
 	ok( !cell.alive, "Cells can die" );
+});
 
+test( "Life cycles", function () {
 	function makeNeighbours(cell, number) {
 		var directions = "n ne e se s sw w nw".split( " " ),
+			neighbours = {},
 			neighbour;
 		while (number--) {
-			neighbour = new Cell();
+			neighbour = new GameOfLife.Cell();
 			neighbour.live();
 			neighbour.age();
-			cell.neighbours[directions[number]] = neighbour;
+			neighbours[directions[number]] = neighbour;
 		}
+		cell.neighbours = neighbours;
 	}
+
+	var cell = new GameOfLife.Cell();
+	makeNeighbours( cell, 3 );
+	cell.nextState();
+	cell.age();
+	ok( cell.alive, "Cells can reproduce" );
+
+	makeNeighbours( cell, randomSample( 0, 1 ) );
+	cell.nextState();
+	cell.age();
+	ok( !cell.alive, "Cells die from under-population" );
+
+	cell = new GameOfLife.Cell();
+	cell.live();
+	cell.age();
+	makeNeighbours( cell, randomSample( 2, 3 ) );
+	cell.nextState();
+	cell.age();
+	ok( cell.alive, "Cells stay alive with the right number of neighbours" );
+
+	makeNeighbours( cell, randomSample( 4, 5, 6, 7, 8 ) );
+	cell.nextState();
+	cell.age();
+	ok( !cell.alive, "Cells die from over-population" );
 });
